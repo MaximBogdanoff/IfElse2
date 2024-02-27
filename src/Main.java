3eb927f347c432a1643856708fe2ca2d9d1dd37b
Максим <bogdanoff.96@yandex.ru>
2024-02-27 19:41:16 +1100

    Completed

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
-import org.w3c.dom.ls.LSOutput;
-
-import java.sql.SQLOutput;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
         int clientOS=1;
         if (clientOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке");
@@ -41,6 +38,49 @@ public class Main {
         else if (deliveryDistance>60 && deliveryDistance<=100) {System.out.println("Потребуется дней: "+(days+2));
     }   else {
             System.out.println("Доставки нет");
+        }
+
+        int monthNumber=12;
+        switch (monthNumber) {
+            case 1:
+                System.out.println("Зима");
+                break;
+            case 2:
+                System.out.println("Зима");
+                break;
+            case 3:
+                System.out.println("Весна");
+                break;
+            case 4:
+                System.out.println("Весна");
+                break;
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+                System.out.println("Лето");
+                break;
+            case 7:
+                System.out.println("Лето");
+                break;
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+                System.out.println("Осень");
+                break;
+            case 10:
+                System.out.println("Осень");
+                break;
+            case 11:
+                System.out.println("Осень");
+                break;
+            case 12:
+                System.out.println("Зима");
+                break;
+            default:
+                System.out.println("Такого месяца не существует");
+
         }
 
 
